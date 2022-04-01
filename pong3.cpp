ece38fc7469bbbeec4e528157982251f9e7446cd
cooldeveloping <45182732+cooldeveloping@users.noreply.github.com>
2022-04-01 13:59:06 -0400

    Countdown thing

--- pong3.cpp
+++ pong3.cpp
@@ -11,6 +11,7 @@ sf::View Mainview(sf::FloatRect(0, 0, 1920, 1080));
 sf::RectangleShape Ball;
 sf::RectangleShape RightPaddle;
 sf::RectangleShape LeftPaddle;
+sf::Font Montserrat;
 
 int main() {
     sf::Clock clock;
@@ -25,9 +26,24 @@ int main() {
     LeftPaddle.setOrigin(sf::Vector2f(15, 100));
     LeftPaddle.setPosition(40, 540);
 
+    float BallCountdown = 3;
+
+    Montserrat.loadFromFile("Pong/Montserrat-Regular.ttf");
+
+    sf::Text countdown;
+
+    countdown.setFont(Montserrat);
+    countdown.setCharacterSize(192);
+    countdown.setString("1");
+    countdown.setOrigin(countdown.getLocalBounds().left + countdown.getLocalBounds().width / 2, countdown.getLocalBounds().top + countdown.getLocalBounds().height / 2);
+    countdown.setPosition(Screenwidth / 2, Screenheight / 2);
+
     while (window.isOpen())
     {
         sf::Time elapsed = clock.restart();
+        if (BallCountdown > 0) {
+            BallCountdown -= elapsed.asSeconds();
+        }
 
         float ScreenRatio = Screenwidth / Screenheight;
         Mainview.setViewport(sf::FloatRect((1.f - (window.getSize().y * ScreenRatio) / (window.getSize().x * 1.f)) / 2, (1.f - (window.getSize().x * 1.f) / (window.getSize().y * ScreenRatio)) / 2, (window.getSize().y * ScreenRatio) / (window.getSize().x * 1.f), (window.getSize().x * 1.f) / (window.getSize().y * ScreenRatio)));
@@ -52,10 +68,16 @@ int main() {
         if (Ball.getPosition().x + dir.x * elapsed.asSeconds() > Screenwidth - 20) {
             dir = sf::Vector2f(-dir.x, dir.y);
         } 
+        if (Ball.getPosition().x + dir.x * elapsed.asSeconds() < -20) {
+            BallCountdown = 3;
+            Ball.setPosition(Screenwidth / 2, Screenheight / 2);
+        } 
         if (Ball.getPosition().y + dir.y * elapsed.asSeconds() > Screenheight - 20 || Ball.getPosition().y + dir.y * elapsed.asSeconds() < 20) {
             dir = sf::Vector2f(dir.x, -dir.y);
         }
-        Ball.move(dir * elapsed.asSeconds());
+        if (BallCountdown < 0) {
+            Ball.move(dir * elapsed.asSeconds());
+        }
 
         sf::FloatRect BallPlusMove(Ball.getGlobalBounds().left + dir.x * elapsed.asSeconds(), Ball.getGlobalBounds().top + dir.y * elapsed.asSeconds(), Ball.getGlobalBounds().width, Ball.getGlobalBounds().height);
         if (BallPlusMove.intersects(LeftPaddle.getGlobalBounds())) {
@@ -65,6 +87,7 @@ int main() {
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
             window.close();
         }
+
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -74,10 +97,15 @@ int main() {
 
         window.clear();
 
-        window.draw(Ball);
         window.draw(LeftPaddle);
         window.draw(RightPaddle);
-
+        if (BallCountdown < 0) {
+            window.draw(Ball);
+        } else {
+            int BallCountdownCeil = std::ceil(BallCountdown);
+            countdown.setString(std::to_string(BallCountdownCeil));
+            window.draw(countdown);
+        }
         window.display();
     }
 }
